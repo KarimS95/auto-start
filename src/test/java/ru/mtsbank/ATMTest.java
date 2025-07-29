583e7e14631fc2b45c2698bf2b72592b2ae13843
Karim S <krs261095@gmail.com>
2025-07-29 13:56:46 +0300

    1-iteration auto Учимся писать тесты

--- src/test/java/ru/mtsbank/ATMTest.java
+++ src/test/java/ru/mtsbank/ATMTest.java
@@ -24,26 +24,53 @@ public class ATMTest {
             case RUB -> ATM.getBalance(RUB);
 
         };
-        Assert.assertEquals(actualResult,expectedResult);
+        Assert.assertEquals(actualResult, expectedResult);
     }
 
     @Test(dataProvider = "balance")
-    public void testGetBalanceAfterAmountCalc(Integer action, BigDecimal expectedResult) {
-        switch (action) {
-            case 1 -> ATM.calcTotalAmount(new DollarBanknote(USD, 100),BigDecimal.valueOf(100),1);
-            case 2 -> ATM.calcTotalAmount(new DollarBanknote(USD, 100),BigDecimal.valueOf(100),2);
+    public void testGetBalanceAfterAmountCalc(Currency currency, Integer action, BigDecimal expectedResult) {
+        switch (currency) {
+
+            case USD -> {
+                switch (action) {
+                    case 1 -> ATM.calcTotalAmount(new DollarBanknote(USD, 100), BigDecimal.valueOf(100), 1);
+                    case 2 -> ATM.calcTotalAmount(new DollarBanknote(USD, 100), BigDecimal.valueOf(100), 2);
+                }
+            }
+            case EUR -> {
+                switch (action) {
+                    case 1 -> ATM.calcTotalAmount(new EuroBanknote(EUR, 100), BigDecimal.valueOf(100), 1);
+                    case 2 -> ATM.calcTotalAmount(new EuroBanknote(EUR, 100), BigDecimal.valueOf(100), 2);
+                }
+            }
+            case RUB -> {
+                switch (action) {
+                    case 1 -> ATM.calcTotalAmount(new RubBanknote(RUB, 100), BigDecimal.valueOf(100), 1);
+                    case 2 -> ATM.calcTotalAmount(new RubBanknote(RUB, 100), BigDecimal.valueOf(100), 2);
+                }
+            }
         }
-        BigDecimal actualResult = ATM.getBalance(USD);
+
+        BigDecimal actualResult = switch (currency) {
+            case USD -> ATM.getBalance(USD);
+            case EUR -> ATM.getBalance(EUR);
+            case RUB -> ATM.getBalance(RUB);
+        };
+
         Assert.assertEquals(actualResult, expectedResult);
     }
 
     @DataProvider(name = "balance")
     public Object[][] balance() {
         return new Object[][]{
-                {1,BigDecimal.valueOf(2100)},
-                {2,BigDecimal.valueOf(2000)}
+                {USD, 1, BigDecimal.valueOf(2100)},
+                {USD, 2, BigDecimal.valueOf(2000)},
+                {EUR, 1, BigDecimal.valueOf(4100)},
+                {EUR, 2, BigDecimal.valueOf(4000)},
+                {RUB, 1, BigDecimal.valueOf(5100)},
+                {RUB, 2, BigDecimal.valueOf(5000)}
         };
-        }
+    }
 
     @Test(dataProvider = "currencyNominalsCount")
     public void testAmountValidator(Currency currency, int expectedResult) {
@@ -59,7 +86,7 @@ public class ATMTest {
 
     @DataProvider(name = "currencies")
     public Object[][] currencies() {
-        return new Object[][] {
+        return new Object[][]{
                 {USD, BigDecimal.valueOf(2000)},
                 {EUR, BigDecimal.valueOf(4000)},
                 {RUB, BigDecimal.valueOf(5000)}
